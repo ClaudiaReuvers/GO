545ec141c5c39c5d03cf82852456698c478c0769
Claudia Reuvers <claudia.reuvers@nvc3146.nedap.local>
2017-01-17 16:34:16 +0100

    Add documentation to Stone

--- src/game/Stone.java
+++ src/game/Stone.java
@@ -4,8 +4,13 @@ public class Stone {
 
 	private StoneColor color;
 	private int liberty;
-	private Stone[] surrounding;
+	//private Stone[] surrounding;
 	
+	/**
+	 * Creates a <code>Stone</code> with a given color.
+	 * @param white <tt>true</tt> if the color is <tt>WHITE</tt>
+	 * <tt>false</tt> if the color is <tt>BLACK</tt>
+	 */
 	public Stone(boolean white) {
 		if (white) {
 			this.color = StoneColor.WHITE;
@@ -14,10 +19,18 @@ public class Stone {
 		}
 	}
 	
+	/**
+	 * Creates an empty <code>Stone</code>.
+	 */
 	public Stone() {
 		this.color = StoneColor.EMPTY;
 	}
 	
+	/**
+	 * Set the color of the <code>Stone</code>.
+	 * @param white <tt>true</tt> if the color is <tt>WHITE</tt>,
+	 * <tt>false</tt> if the color is <tt>BLACK</tt>
+	 */
 	public void setColor(boolean white) {
 		if (white) {
 			this.color = StoneColor.WHITE;
@@ -26,6 +39,11 @@ public class Stone {
 		}
 	}
 	
+	/**
+	 * Decreases the <tt>liberty</tt> of this <code>Stone</code> with one.
+	 * @requires getLiberty() > 0
+	 * @ensures getLiberty() == old(getLiberty()) - 1
+	 */
 	public void decreaseLibertyByOne() {
 		if (liberty > 0) {
 			liberty--;
@@ -42,13 +60,18 @@ public class Stone {
 	}
 	
 	/**
+	 * Returns if the <code>Stone</code> is empty.
 	 * @pure
-	 * @return
+	 * @return <tt>true</tt> if getColor() == StoneColor.EMPTY
 	 */
 	public boolean isEmpty() {
 		return (color == StoneColor.EMPTY);
 	}
-	
+
+	/**
+	 * Returns if the <tt>liberty</tt> of this <code>Stone</code> is zero.
+	 * @return <tt>true</tt> if getLiberty() == 0
+	 */
 	public boolean noLiberty() {
 		/*
 		for (int i = 0; i < surrounding.length; i++) {
@@ -58,20 +81,38 @@ public class Stone {
 		}
 		return true;
 		*/
-		return liberty == 0;
+		return (this.liberty == 0);
 	}
 	
+	/**
+	 * Sets the number of liberties of the <code>Stone</code>.
+	 * @param liberty number of liberties the <code>Stone</code> has
+	 */
 	public void setLiberty(int liberty) {
 		this.liberty = liberty;
 	}
 	
-	public StoneColor getColor() {
-		return color;
+	/**
+	 * Returns the number of liberties of the <code>Stone</code>.
+	 * @return the number of liberties of the <code>Stone</code>
+	 */
+	public int getLiberty() {
+		return this.liberty;
 	}
 	
 	/**
+	 * Returns the color of the <code>Stone</code>.
+	 * @return the color of the <code>Stone</code>
+	 */
+	public StoneColor getColor() {
+		return this.color;
+	}
+	
+	/**
+	 * Returns if the <tt>color</tt> of the <code>Stone</code> is <tt>BLACK</tt> or <tt>WHITE</tt>
 	 * @requires !isEmpty()
-	 * @return
+	 * @return <tt>true</tt> if the <code>Stone</code> is <tt>WHITE</tt>,
+	 * <tt>false</tt> is the <code>Stone</code> is <tt>BLACK</tt>
 	 */
 	public boolean getBooleanColor() {
 		if (color == StoneColor.WHITE) {
