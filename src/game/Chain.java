c13cd6f1288edaf501627d42c15a8da50ff3d512
Claudia Reuvers <claudia.reuvers@nvc3146.nedap.local>
2017-01-18 13:38:08 +0100

    Add Chain

---
+++ src/game/Chain.java
@@ -0,0 +1,39 @@
+package game;
+
+import java.util.ArrayList;
+
+public class Chain {
+
+	private ArrayList<Stone> chain;
+	
+	public Chain() {
+		chain = new ArrayList<>();
+	}
+	
+	public ArrayList<Stone> getChain() {
+		return chain;
+	}
+	
+	public int getLength() {
+		return chain.size();
+	}
+	
+	public void add(Stone stone) {
+		stone.addChain(this);
+		chain.add(stone);
+	}
+	
+	public void join(Chain secondChain) {
+		for (Stone stone : secondChain.getChain()) {
+			chain.add(stone);
+		}
+	}
+	
+	public int getLibertyChain() {
+		int liberty = 0;
+		for (Stone stone: chain) {
+			liberty += stone.getLiberty();
+		}
+		return liberty;
+	}
+}
