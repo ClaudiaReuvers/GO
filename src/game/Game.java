08f754e862ed96c82ce91be53d63f322ec24ffaa
Claudia Reuvers <claudia.reuvers@nvc3146.nedap.local>
2017-01-20 17:17:38 +0100

    Add Game

--- src/game/Game.java
+++ src/game/Game.java
@@ -4,13 +4,32 @@ import communication.Client;
 
 public class Game {
 
-	private Client client1;
-	private Client client2;
+	private Client[] clients;
 	private Board board;
 	
-	public Game(Client client1, Client client2, int dimension) {
-		this.client1 = client1;
-		this.client2 = client2;
+	public Game(Client[] clients, int dimension) {
+		this.clients = clients;
 		board = new Board(dimension);
 	}
+	
+	public String[] getClientNames() {
+		String[] names = new String[clients.length];
+		for (int i = 0; i < clients.length; i++) {
+			names[i] = clients[i].getName();
+		}
+		return names;
+	}
+	
+	public int getNrClients() {
+		return clients.length;
+	}
+	
+	public Client[] getClients() {
+		return clients;
+	}
+	
+	public Board getBoard() {
+		return board;
+	}
+	
 }
